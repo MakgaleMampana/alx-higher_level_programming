d0f69d87905ca4458b4c224264c3708fba70bef2
MakgaleMampana <makgale.mampana94+git@gmail.com>
2023-07-13 14:06:04 +0000

    sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
-sts all records of the table ordered by score (top first) displaying only score and name.
-SELECT `score`, `name` FROM second_table ORDER BY `score` DESC;
+--a script that lists all records of the table second_table of the database hbtn_0c_0
+SELECT score, name FROM second_table ORDER BY score DESC;
